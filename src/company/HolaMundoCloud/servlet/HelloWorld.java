acaa967cb58c988494cd17ed2390f1d3b48d2b87
ironwill <35231658+josueAlba@users.noreply.github.com>
2019-02-13 15:26:18 -0500

    Add files via upload

---
+++ src/company/HolaMundoCloud/servlet/HelloWorld.java
@@ -0,0 +1,47 @@
+package company.HolaMundoCloud.servlet;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out=response.getWriter();
+		double sqrtvalue=Math.sqrt(4);
+		
+				out.println("<html><body> <h1 align='center'>Hola mundo Cloud</h1>"
+						+ "<br>La raiz de 4 es: "+sqrtvalue+"<br>"
+						+"</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
